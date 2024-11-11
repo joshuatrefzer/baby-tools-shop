87fb6a5467d4372679e5f9de11f0b0a387435494
joshuatrefzer <121818554+joshuatrefzer@users.noreply.github.com>
2024-11-11 16:50:44 +0100

    add media files

--- babyshop_app/babyshop/urls.py
+++ babyshop_app/babyshop/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('products.urls')),
     path('users/',include('users.urls')),
-] 
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
